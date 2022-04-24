7cbfa6de68d1f903f459f1eed75232b30d77049c
sittidetPawutinan <oddsmac@sittidetPawutinans-MacBook-Air.local>
2022-04-24 11:26:29 +0700

    edit app.component.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('odds-coming-soon app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('oming soon by @JJ');
   });
 });
